7edbed8bd8281497655488360f42d9551f277c6f
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-10-04 17:41:01 +0200

    Update pixelos.sh

--- scripts/pixelos.sh
+++ scripts/pixelos.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests ; \
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen -g default,-mips,-darwin,-notdefault && \
-git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-pos14 .repo/local_manifests && \
+git clone https://github.com/peternmuller/android_build_manifest.git -b apollo-pos14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 source build/envsetup.sh && \
 lunch aosp_apollo-ap2a-user && \
