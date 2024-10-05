1ab22fa4f13435de7c3e030c933a8732106b72f8
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-10-05 16:45:16 +0200

    Update aospa.sh

--- scripts/aospa.sh
+++ scripts/aospa.sh
@@ -1,9 +1,7 @@
 rm -rf .repo/local_manifests && \
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b uvite -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/peternmuller/android_build_manifest.git -b apollo-aospa14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
-git clone https://github.com/AOSPA/android_device_xiaomi_apollo.git -b uvite device/xiaomi/apollo && \
-git clone https://github.com/ThankYouMario/proprietary_vendor_xiaomi.git -b uvite vendor/xiaomi/apollo && \
-git clone https://github.com/AOSPA/android_kernel_xiaomi_sm8250.git -b uvite kernel/xiaomi/sm8250 && \
 source build/envsetup.sh && \
 lunch aospa_apollo-user && \
 make installclean && \
