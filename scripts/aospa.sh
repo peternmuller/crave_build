abdefe7d1c979bb86ba7f449cdff419502512f7c
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-10-06 12:58:44 +0200

    Update aospa.sh

--- scripts/aospa.sh
+++ scripts/aospa.sh
@@ -1,5 +1,4 @@
 rm -rf .repo/local_manifests && \
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b uvite -g default,-mips,-darwin,-notdefault && \
-git clone https://github.com/peternmuller/android_build_manifest.git -b apollo-aospa14 .repo/local_manifests && \
 /opt/crave/resync.sh && \
 ./rom-build.sh apollo
