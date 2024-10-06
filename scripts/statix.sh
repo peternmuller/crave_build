94d3f7b5b43b552f647a2afb325cdde067c85b93
Peter Noël Muller <100597184+peternmuller@users.noreply.github.com>
2024-10-07 00:01:32 +0200

    Update statix.sh

--- scripts/statix.sh
+++ scripts/statix.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests ; \
-repo init --depth=1 --no-repo-verify -u https://github.com/StatiXOS/android_manifest.git -b udc-qpr3 -g default,-mips,-darwin,-notdefault && \
-git clone https://github.com/aosp-realm/android_build_manifest.git -b apollo-statix .repo/local_manifests && \
+repo init --depth=1 --no-repo-verify -u https://github.com/StatiXOS/android_manifest.git -b vic -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/peternmuller/android_build_manifest.git -b apollo-statix .repo/local_manifests && \
 /opt/crave/resync.sh && \
 sed -i '/struct sigaction {/,/};/d' hardware/qcom-caf/sm8250/kernel-headers/asm-generic/signal.h && \
 source build/envsetup.sh && \
